6c245d80b0bcd833e6c3ae6ea26e9b6aa5b77383
MMD <m.mohammad.7521@gmail.com>
2022-02-20 20:15:01 +0330

    First practice

---
+++ src/main/java/A/StreamA.java
@@ -0,0 +1,32 @@
+package A;
+
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+
+public class StreamA {
+
+    public static Map<Integer,List<String>> groupByLength(List<String> strings){
+        return strings.stream().distinct()
+                .collect(Collectors.groupingBy(String::length));
+    }
+
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("enter names (Divided by space):");
+        List<String> nameList = new ArrayList<>();
+        nameList.addAll(Arrays.asList(scanner.nextLine().split(" ")));
+
+        System.out.println("names have been added to the list!");
+        System.out.println();
+
+        System.out.println(groupByLength(nameList));
+
+
+    }
+}
---
+++ src/main/java/B/StreamB.java
@@ -0,0 +1,10 @@
+package B;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class StreamB {
+
+}
